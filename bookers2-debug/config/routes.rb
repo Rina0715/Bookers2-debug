f36a1fc5ba128fd5f4ee3551186fae2bc6cfadea
Rina0715 <rk19910715@gmail.com>
2021-05-15 09:15:28 +0000

    [Finish]Bookers2-search

--- bookers2-debug/config/routes.rb
+++ bookers2-debug/config/routes.rb
@@ -1,20 +1,19 @@
 Rails.application.routes.draw do
+  get 'search/search'
   root to: 'homes#top'
   devise_for :users
   resources :books do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:index, :create, :destroy]
   end
+
   resources :users do
-  #   member do
-  #     get :following, :followers
-  #   end
-  # end
-  # resources :relationships, only: [:create, :destroy]
     resource :relationships, only: [:create, :destroy]
     get 'followings' => 'relationships#followings', as: 'followings'
     get 'followers' => 'relationships#followers', as: 'followers'
   end
+
+  get '/search' => 'search#search'
 
 
   get 'home/about' => 'homes#about'
